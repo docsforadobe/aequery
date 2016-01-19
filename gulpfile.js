534c9a49eb078b32a3402cccc93b47b6c5d05e1c
Klustre <Remco@remco-4.speedport_w_724v_typ_a_05011603_00_007>
2016-01-19 20:18:12 +0100

    Adding gulp

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var gulp = require('gulp'),
+	os = require('os'),
+	del = require('del'),
+	concat = require('gulp-concat');
+
+var name = 'aequery',
+	dest;
+
+// Expects you have After Effects CC 2015 installed in the default location
+if (os.platform() == 'darwin') {
+	console.log('OS: Mac OS X (darwin)');
+	dest = '/Volumes/Macintosh HD/Applications/Adobe After Effects CC 2015/Scripts/ScriptUI Panels/' + name;
+} else {
+	console.log('OS: Windows (win32)');
+	dest = 'C:/Program Files/Adobe/Adobe After Effects CC 2015/Support Files/Scripts/ScriptUI Panels/' + name;
+}
+
+//	Or define your own path:
+//	dest = '/Your/Path/To/Adobe After Effects CC 2015/Scripts/ScriptUI Panels/' + name;
+
+gulp.task('clean', function () {
+	return del([dest], {
+		force: true
+	});
+});
+
+gulp.task('clean:build', function () {
+	return del(['build'], {
+		force: true
+	});
+});
+
+gulp.task('concat', ['clean'], function () {
+	var stream = gulp.src('lib/*.js')
+		.pipe(concat(name + '.js'))
+		.pipe(gulp.dest(dest));
+	console.log('Wrote ' + name + '.js to: \r\n' + dest);
+	return stream;
+});
+
+gulp.task('concat:build', ['clean'], function () {
+	var stream = gulp.src('lib/*.js')
+		.pipe(concat(name + '.js'))
+		.pipe(gulp.dest('build'));
+	console.log('Wrote ' + name + '.js to: \r\n/build');
+	return stream;
+});
+
+gulp.task('deploy', ['concat']);
+
+gulp.task('build', ['concat:build']);
+
+gulp.task('default', ['deploy']);
