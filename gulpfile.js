6cfa9649b3ae4e4685d5a9a55176c8bb018cd524
Klustre <Remco@remco-4.speedport_w_724v_typ_a_05011603_00_007>
2016-01-20 23:27:07 +0100

    Improve Gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,20 @@
 var gulp = require('gulp'),
 	os = require('os'),
 	del = require('del'),
+	queue = require('streamqueue'),
 	concat = require('gulp-concat'),
+	zip = require('gulp-zip'),
+	pdf = require('gulp-markdown-pdf'),
 	uglify = require('gulp-uglify');
 
-var name = 'aequery',
+var pkg = require('./package.json'),
+	name = pkg.name,
 	lib = ['lib/main.js', 'lib/*.js', '!lib/(main)*.js'],
 	dest;
 
-// Expects you have After Effects CC 2015 installed in the default location
+/* Expects you have After Effects CC 2015 installed 
+ * in the default location 
+ */
 if (os.platform() == 'darwin') {
 	console.log('OS: Mac OS X (darwin)');
 	dest = '/Applications/Adobe After Effects CC 2015/Scripts/ScriptUI Panels/' + name;
@@ -19,35 +25,75 @@ if (os.platform() == 'darwin') {
 	dest = 'C:/Program Files/Adobe/Adobe After Effects CC 2015/Support Files/Scripts/ScriptUI Panels/' + name;
 }
 
-//	Or define your own path:
+
+/* Or define you own path by uncommenting
+ * and changing the path below: 
+ */
 //	dest = '/Your/Path/To/Adobe After Effects CC 2015/Scripts/ScriptUI Panels/' + name;
 
+
+// DEFAULT TASK
+gulp.task('default', ['debug', 'watch']);
+
+// WATCH
 gulp.task('watch', function () {
-	gulp.watch('lib/*.js', ['concat']);
+	gulp.watch('lib/*.js', ['debug']);
 });
 
+// CLEAN
 gulp.task('clean', function () {
 	return del([dest], {
 		force: true
 	});
 });
 
-gulp.task('clean:build', function () {
-	return del(['build'], {
-		force: true
-	});
-});
-
-gulp.task('concat', ['clean'], function () {
+// DEBUG
+gulp.task('debug', ['clean'], function () {
 	var stream = gulp.src(lib)
 		.pipe(concat(name + '.js'))
-		// .pipe(uglify())
 		.pipe(gulp.dest(dest));
 	console.log('Wrote ' + name + '.js to: \r\n' + dest);
 	return stream;
 });
 
-gulp.task('concat:build', ['clean'], function () {
+// RELEASE
+var div = '-',
+	ugliness = {};
+
+gulp.task('release', ['clean'], function () {
+	var stream = queue({ objectMode: true });
+		
+	stream.queue(
+		gulp.src(lib)
+			.pipe(concat(name + '.js'))
+			.pipe(uglify())
+			.pipe(gulp.dest(dest))
+	);
+	
+	stream.queue(
+		gulp.src('README.md')
+			.pipe(pdf())
+	);
+		
+	console.log('Wrote ' + name + '.js to: \r\n' + dest);
+		
+	return stream.done()
+		.pipe(zip(pkg.name + div + pkg.version + div + now() + '.zip'))
+		.pipe(gulp.dest('dist'));
+});
+
+function now() {
+	var div = "";
+	var date = new Date(),
+		month = date.getMonth()+1,
+		day = date.getDay(),
+		hrs = date.getHours(),
+		min = date.getMinutes();
+	return month + div + day + div + hrs + div + min;
+}
+
+// BUILD - when you want a concat version in the root
+gulp.task('build', ['clean:build'], function () {
 	var stream = gulp.src(lib)
 		.pipe(concat(name + '.js'))
 		.pipe(gulp.dest('build'));
@@ -55,8 +101,9 @@ gulp.task('concat:build', ['clean'], function () {
 	return stream;
 });
 
-gulp.task('deploy', ['concat']);
-
-gulp.task('build', ['concat:build']);
-
-gulp.task('default', ['deploy', 'watch']);
+// CLEAN BUILD
+gulp.task('clean:build', function () {
+	return del(['build'], {
+		force: true
+	});
+});
