cd0ce759653fcce7600b2416e51a09a39f05e8dd
Rune Gangsø <runegan@gmail.com>
2016-01-22 23:11:47 +0100

    Create basic test UI

--- testproject/extendscript/aeq_test.jsx
+++ testproject/extendscript/aeq_test.jsx
@@ -3,18 +3,21 @@
 
 function buildUI(win)
 {
+	var winObj = win.get();
+	winObj.alignment = ['fill', 'fill'];
+	winObj.alignChildren = ['fill', 'fill'];
 	var txtInput = win.addEditText('Enter a value');
 
-	var btn1 = win.addButton('Button 1', function(e) { 
-		alert(txtInput.text);
+	var btn1 = win.addButton('Button 1', function(e) {
+		alert(aeq(txtInput.text));
 	});
 
-	var btn2 = win.addButton('Button 2', function(e) { 
+	var btn2 = win.addButton('Button 2', function(e) {
 		alert("Hello world");
 	});
 }
 
-aeq.ui.ready(function() 
+aeq.ui.ready(function()
 {
 	var win = aeq.ui.createMainWindow("Hello world!");
 
