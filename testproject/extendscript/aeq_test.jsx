17cf2d7dfddd941c71ec8c42bdedcdcadce38121
Rafi Khan <rafi@spotonparking.com>
2016-01-24 15:22:33 -0800

    Temp commit so I can pull

--- testproject/extendscript/aeq_test.jsx
+++ testproject/extendscript/aeq_test.jsx
@@ -5,12 +5,20 @@ function buildUI(win)
 {
 	var txtInput = win.addEditText('Enter a value');
 
-	var btn1 = win.addButton('Button 1', function(e) { 
-		alert(txtInput.text);
+	var btn1 = win.addButton('Button 1', function(e) 
+	{ 
+		// aeq.getActiveComposition().getSelectedProperties().forEach(function(prop) 
+		// {
+		// 	aeq(prop).getSelectedKeys(function(aeqKey)
+		// 	{
+				
+		// 	});
+		// });
+
 	});
 
 	var btn2 = win.addButton('Button 2', function(e) { 
-		alert("Hello world");
+		alert(aeq.reflect({ hello : 'world' }));
 	});
 }
 
