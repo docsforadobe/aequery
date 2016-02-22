21a02b6ae2be9baa97bab9a04a06115af1c3e6ee
Rune Gangsø <runegan@gmail.com>
2016-02-22 21:08:15 +0100

    Add creating with objects demo

--- testproject/extendscript/aeq-ui_Demo.jsx
+++ testproject/extendscript/aeq-ui_Demo.jsx
@@ -60,13 +60,97 @@ function buildUI(win) {
 
 }
 
+function buildUIWithObjects(win) {
+	win.set({
+		alignment: ['fill', 'top'],
+		alignChildren: ['fill', 'top']
+	});
+	win.addStatictext({
+		text: 'Statictext text'
+	});
+	win.addEditText({text: 'EditText text'});
+	win.addButton({
+		text: 'Button Name',
+		onClick: function() {
+			alert('Button clicked');
+		}
+	});
+	win.addCheckbox({
+		text: 'Checkbox Name',
+		onClick: function() {
+			alert(this.value);
+		}
+	});
+	win.addRadioButton({
+		text: 'Radiobutton text',
+		onClick: function() {
+			alert(this.value);
+		}
+	});
+	win.addDropdownList({
+		items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
+		onChange: function() {
+			alert(this.selection);
+		}
+	});
+	win.addListbox({
+		items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
+		properties: {
+			multiselect: true,
+			numberOfColumns: 1
+		},
+		onChange: function() {
+			alert(this.selection);
+		}
+	});
+
+	var buttonGroup = win.addGroup({
+		alignChildren : ['fill', 'top']
+	});
+	buttonGroup.addButton({ text: 'Button 1' });
+	buttonGroup.addButton({ text: 'Button 2' });
+	buttonGroup.addButton({ text: 'Button 3' });
+
+	var buttonPanel = win.addPanel({
+		text: 'Panel Name',
+		alignChildren: 'fill'
+	});
+	buttonPanel.addButton({ text: 'Button 1' });
+	buttonPanel.addButton({ text: 'Button 2' });
+	buttonPanel.addButton({ text: 'Button 3' });
+
+	var tabbedpanel = win.addTabbedPanel();
+	var tab1 = tabbedpanel.addTab('Tab 1');
+	tab1.addButton('Tab 1 Button');
+	var tab2 = tabbedpanel.addTab('Tab 2');
+	tab2.addButton('Tab 2 Button');
+
+	win.addProgressbar(50);
+
+	var treeview = win.addTreeview({
+		items: ['Item 1', 'Item 2'],
+		onChange: function() {
+			alert(this.selection);
+		}
+	});
+	var treeviewNode = treeview.add('node', 'item 3');
+	treeviewNode.add('item', 'item 3.1');
+
+	win.addSlider(0, 100, 200);
+	win.addScrollbar();
+}
+
 aeq.ui.ready(function()
 {
-	var win = aeq.ui.createMainWindow("Panel Name");
+	var win = aeq.ui.createMainWindow("With Strings");
 
 	buildUI(win);
 
 	win.show();
+
+	var win2 = aeq.ui.createWindow("With objects");
+	buildUIWithObjects(win2);
+	win2.show();
 });
 function redefineAeq(aeqFile, aeqUiFile) {
 	$.evalFile(aeqFile);
