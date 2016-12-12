a97265d498927ba01268a9db44590522eab3c874
Rune Gangsø <runegan@gmail.com>
2016-12-12 19:54:18 +0100

    Add ArrayEx.attr and aeq.attr function
    Description:
    	Gets or sets values on all objects in an array. When getting a value,
    	only the value of the first object will be returned
    
    	This can also be used to do a method on all objects in the array,
    	but currently only when newValue is given. When newValue is not given
    	the method will only be executed on the first object in the array.
    
    Arguments:
    	- array: array of objects to get or set attribute from/to.
    	- attributeName: string with name of attribute to get or set
    	- newValue: optional. The new value to set
    
    Returns:
    	When getting: the value of attributeName for the first element in array
    	When setting: array
    
    Examples:
    	Enable all disabled layers:
    		aeq("layer:not(enabled)").attr("enabled", true);
    	Get name of first disabled layer
    		aeq("layer:not(enabled)").attr("name");
    	Set value of properties in an array:
    		aeq.attr(arrayOfProps, "value", 100);
    
    	Using a method to copy all disabled layers to targetComp:
    		aeq("layer:not(enabled)").attr("copyToComp", targetComp);

---
+++ test/attr.js
@@ -0,0 +1,46 @@
+( function() {
+#include "../dist/aeq.js"; // jshint ignore:line
+
+aeq.undoGroup( "test attr", function() {
+	setup();
+	var log = [];
+
+	// Test layer and simple attribute
+	var disabledLayers = aeq( "layer:not(enabled)" );
+
+	var allAreNotEnabled = disabledLayers.isTrueForAll( function( layer ) {
+		return layer.enabled === false;
+	} );
+	log.push( "All layers are disabled = " + allAreNotEnabled );
+
+	var firstLayerEnabled = disabledLayers.attr( "enabled" );
+	log.push( "First layer is enabled = " + firstLayerEnabled );
+
+	disabledLayers.attr( "enabled", true );
+	firstLayerEnabled = disabledLayers.attr( "enabled" );
+	log.push( "First layer is enabled = " + firstLayerEnabled );
+
+	// Test Property and special attribute
+
+	var positionProps = aeq( "prop[name='Rotation']" );
+
+	var firstValue = positionProps.attr( "value" );
+	log.push( "First value = " + firstValue );
+
+	positionProps.attr( "value", 50 );
+
+	firstValue = positionProps.attr( "value" );
+	log.push( "First value = " + firstValue );
+
+	alert( "Log:\n" + log.join("\n") );
+} );
+
+function setup() {
+	var comp = aeq.comp.create( { name: "test attr" } );
+	for ( var i = 0; i < 8; i++ ) {
+		var layer = comp.layers.addShape();
+		layer.enabled = i % 2 === 0;
+	}
+	comp.openInViewer();
+}
+}() );
