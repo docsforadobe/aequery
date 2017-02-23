94288a75bcca8323c62c258369a2b66c3b48a082
Rune Gangsø <runegan@gmail.com>
2017-02-23 01:00:39 +0100

    Add tests for assert.js

---
+++ test/assert.js
@@ -0,0 +1,48 @@
+( function() {
+//@include "../dist/aeq.js"
+
+var testName = "Test assert";
+
+aeq.undoGroup( testName, function() {
+	var errors = [];
+
+	var tests = [
+		["assertIsNull", [null, "error"], false],
+		["assertIsNull", [{}, "error"], true],
+		["assertIsNotNull", [null, "Error"], true],
+		["assertIsNotNull", [{}, "Error"], false]
+	];
+
+	for (var i = 0; i < tests.length; i++) {
+		var test = tests[i];
+		try {
+			var result = aeq[test[0]].apply(null, test[1]);
+		} catch( e ) {
+			var shouldThrowError = test[2];
+			if (!shouldThrowError) {
+				errors.push({test: test, result: result});
+			}
+		}
+	}
+
+	var e = testName + ": Errors:\n";
+	for (i = 0; i < errors.length; i++) {
+		var error = errors[i];
+		e += "\n";
+		var obj = error.test[1] !== undefined && error.test[1] !== null ?
+			error.test[1].toString() :
+			'undefined';
+
+		result = error.result !== undefined ? result.toSource() : 'undefined';
+		e += error.test[0] + " failed when passed " + obj +
+			" it returned " + result + " not " + error.test[2].toSource();
+	}
+
+	if ( errors.length !== 0) {
+		alert( e );
+	} else {
+		alert( testName + ": No errors!" );
+	}
+} );
+
+}() );
