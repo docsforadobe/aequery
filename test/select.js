aa5181a6bf8a33fe442d9674047fd90a7c0ae5c9
Rune Gangsø <runegan@gmail.com>
2017-03-02 00:16:37 +0100

    Select: get activecomp using selector

--- test/select.js
+++ test/select.js
@@ -1,7 +1,8 @@
 ( function() {
-#include "../dist/aeq.js"; // jshint ignore:line
+//@include "../dist/aeq.js";
 
 var comp = aeq.comp.create({});
+comp.openInViewer();
 var layer = comp.layers.addShape();
 for ( var i = 0; i < 5; i++ ) {
 	var newLayer = layer.duplicate();
@@ -17,6 +18,11 @@ var select2 = aeq( "layer[name=['layerName 0', 'layerName 2']]");
 if ( select2.length !== 2 ) {
 	alert( "select2 did not select 2" );
 }
+
+if ( aeq( 'activeComp' )[0] !== comp ) {
+	alert( 'activecomp did not get activecomp' );
+}
+
 comp.remove();
 alert( "Done checking select.js" );
 }() );
