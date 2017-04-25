b95ee9a6bcbe17678dbf054f4116abaf186c2d59
Rune Gangsø <runegan@gmail.com>
2017-04-26 00:31:11 +0200

    Selector: support for 'item'
    resolves: #23

--- test/select.js
+++ test/select.js
@@ -1,7 +1,7 @@
 ( function() {
 //@include "../dist/aeq.js";
 
-var comp = aeq.comp.create({});
+var comp = aeq.comp.create({ name: 'Test: selector'});
 comp.openInViewer();
 var layer = comp.layers.addShape();
 for ( var i = 0; i < 5; i++ ) {
@@ -16,12 +16,12 @@ if ( select4.length !== 4 ) {
 
 var select1 = aeq( "layer[name=/^Shape/]");
 if ( select1.length !== 1 ) {
-	alert( "select1 did not select 1" );
+	alert( "layer[name=/^Shape/] did not select 1" );
 }
 
 var select1 = aeq( "layer:has(name=/^Shape/)");
 if ( select1.length !== 1 ) {
-	alert( "select1 did not select 1" );
+	alert( "layer:has(name=/^Shape/) did not select 1" );
 }
 
 var select2 = aeq( "layer[name=['layerName 0', 'layerName 2']]");
@@ -31,6 +31,11 @@ if ( select2.length !== 2 ) {
 
 if ( aeq( 'activeComp' )[0] !== comp ) {
 	alert( 'activecomp did not get activecomp' );
+}
+
+select1 = aeq( "item[name='" + comp.name + "']");
+if ( select1.length !== 1 ) {
+	alert( "item[name='" + comp.name + "'] did not select 1" );
 }
 
 comp.remove();
