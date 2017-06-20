87e50d4c485f673971a0c924e4a0aa0c5b356522
Rune Gangsø <runegan@gmail.com>
2017-06-21 00:38:49 +0200

    Docs: rebuild docs

--- docs/scripts/linenumber.js
+++ docs/scripts/linenumber.js
@@ -1,81 +1,25 @@
-(function (w) {
-	'use strict';
+/*global document */
+(function() {
+    var source = document.getElementsByClassName('prettyprint source linenums');
+    var i = 0;
+    var lineNumber = 0;
+    var lineId;
+    var lines;
+    var totalLines;
+    var anchorHash;
 
-	if (typeof w.hljs === 'undefined') {
-		console.error('highlight.js not detected!');
-	} else {
-		w.hljs.initLineNumbersOnLoad = initLineNumbersOnLoad;
-		w.hljs.lineNumbersBlock = lineNumbersBlock;
-	}
+    if (source && source[0]) {
+        anchorHash = document.location.hash.substring(1);
+        lines = source[0].getElementsByTagName('li');
+        totalLines = lines.length;
 
-	function initLineNumbersOnLoad () {
-		if (document.readyState === 'complete') {
-			documentReady();
-		} else {
-			w.addEventListener('DOMContentLoaded', documentReady);
-		}
-	}
-
-	function documentReady () {
-		try {
-			var blocks = document.querySelectorAll('code.hljs');
-
-			for (var i in blocks) {
-				if (blocks.hasOwnProperty(i)) {
-					lineNumbersBlock(blocks[i]);
-				}
-			}
-		} catch (e) {
-			console.error('LineNumbers error: ', e);
-		}
-	}
-
-  function selectActive(){
-    var anchorHash = document.location.hash.substring(1)
-    document.getElementById(anchorHash)
-      .className = 'selected'
-  }
-
-	function lineNumbersBlock (element) {
-		if (typeof element !== 'object') return;
-
-		var parent = element.parentNode;
-		var lines = getCountLines(parent.textContent);
-
-		if (lines > 1) {
-			var l = '<ul>';
-			for (var i = 1; i < lines+1; i++) {
-				l += '<li class="linenum-item" id="line'+i+'"><a href="#line'+i+'">' + i + '</a></li>';
-			}
-      l+= '</ul>'
-
-			var linesPanel = document.createElement('code');
-			linesPanel.className = 'hljs hljs-line-numbers';
-			linesPanel.style.float = 'left';
-			linesPanel.innerHTML = l;
-      document.getElementsByClassName('linenum-item')
-        .forEach(function(e){
-          e.addEventListener("click", function(event){
-            event.preventDefault();
-            document.location.hash=e.id
-          })
-      })
-			parent.insertBefore(linesPanel, element);
-      selectActive()
-		}
-	}
-
-	function getCountLines(text) {
-		if (text.length === 0) return 0;
-
-		var regExp = /\r\n|\r|\n/g;
-		var lines = text.match(regExp);
-		lines = lines ? lines.length : 0;
-
-		if (!text[text.length - 1].match(regExp)) {
-			lines += 1;
-		}
-
-		return lines;
-	}
-}(window));
+        for (; i < totalLines; i++) {
+            lineNumber++;
+            lineId = 'line' + lineNumber;
+            lines[i].id = lineId;
+            if (lineId === anchorHash) {
+                lines[i].className += ' selected';
+            }
+        }
+    }
+})();
