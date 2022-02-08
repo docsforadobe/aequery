b3db4ed39e9589d64c9283ff7dafe708ad89c0c4
Klustre <subs@klustre.nl>
2022-02-08 10:46:50 +0100

    Adds more tests

--- test/types.js
+++ test/types.js
@@ -12,6 +12,12 @@ aeq.undoGroup( testName, function () {
 	for ( i = 0; i < tests.length; i++ ) {
 		var test = tests[i];
 		result = aeq[test.method]( test.value );
+		$.writeln( test.method );
+		$.writeln( test.value );
+		$.writeln( test.expect );
+		$.writeln( result );
+		$.writeln( test.expect === result ? 'Success' : 'Failed' );
+		$.writeln();
 		if ( result !== test.expect ) {
 			errors.push({ test: test, result: result });
 		}
@@ -37,6 +43,13 @@ aeq.undoGroup( testName, function () {
 	}
 });
 
+function getItem( name ) {
+	return aeq.getItems()
+		.filter( function ( item ) {
+			return item.name === name;
+		})[0];
+}
+
 function setup( name, duration ) {
 	var basePath = new File( $.fileName ).parent.toString();
 	var items = aeq.importFiles( [
@@ -48,7 +61,9 @@ function setup( name, duration ) {
 	var ph = items[3];
 	ph.replaceWithPlaceholder( 'placeholder', 100, 100, 24.0, 1.0 );
 	var comp = aeq.comp.create({ name: name, duration: duration || 3 });
+	var comp2 = aeq.comp.create({ name: 'comp', duration: duration || 3 });
 	comp.renderer = 'ADBE Ernst'; // Cinema4D renderer
+	comp.layers.add( comp2 );
 	var adj = comp.layers.addShape();
 	adj.name = 'adjustment';
 	adj.adjustmentLayer = true;
@@ -62,6 +77,12 @@ function setup( name, duration ) {
 	tm.name = 'hasTrackMatte';
 	tm.trackMatteType = TrackMatteType.ALPHA;
 	comp.layers.addNull().name = 'isTrackMatte';
+	var ml = comp.layers.addSolid( [ 0, 0, 0 ], 1080, 1920, 1, 1, 1 );
+	ml.name = 'masked';
+	var shape = new Shape();
+	shape.vertices = [[ 0, 0 ], [ 0, 100 ], [ 100, 100 ], [ 100, 0 ]];
+	var mask = ml.property( 'ADBE Mask Parade' ).addProperty( 'ADBE Mask Atom' );
+	mask.property( 'ADBE Mask Shape' ).setValue( shape );
 	comp.layers.add( items[0] ).name = 'jpg';
 	comp.layers.add( items[1] ).name = 'psd';
 	comp.layers.add( items[2] ).name = 'c4d';
@@ -112,26 +133,38 @@ function getTests( comp ) {
 
 		// TODO: isEmpty
 
-		// TODO: Fails
-		// [ 'isAeq', aeq( 'comp' ), true ],
-		// [ 'isAeq', new aeq.Comp(), true ],
-		// [ 'isAeq', new aeq.Layer(), true ],
-		// [ 'isAeq', new aeq.Property(), true ],
-		// [ 'isAeq', {}, false ],
-		// [ 'isAeq', 1, false ],
+		{ method: 'isAeq', value: aeq( 'comp' ), expect: true },
+		{ method: 'isAeq', value: new aeq.Comp(), expect: true },
+		{ method: 'isAeq', value: new aeq.Layer(), expect: true },
+		{ method: 'isAeq', value: new aeq.Property(), expect: true },
+		{ method: 'isAeq', value: {}, expect: false },
+		{ method: 'isAeq', value: 1, expect: false },
 
 		{ method: 'isApp', value: app, expect: true },
 		{ method: 'isApp', value: app.project, expect: false },
+		{ method: 'isApp', value: {}, expect: false },
 
-		// TODO: isFolder
-		// TODO: isFile
-		// TODO: isFolderItem
-		// TODO: isFootageItem
+		{ method: 'isFolder', value: new Folder(), expect: true },
+		{ method: 'isFolder', value: getItem( 'Solids' ), expect: false },
+		{ method: 'isFolder', value: getItem( 'aeq.jpg' ), expect: false },
+		{ method: 'isFolder', value: {}, expect: false },
 
-		// TODO: Fails
-		// { method: 'isComp', value: comp, expect: true },
-		// { method: 'isComp', value: comp.layer( 1 ), expect: false },
-		// { method: 'isComp', value: {}, expect: false },
+		{ method: 'isFolderItem', value: getItem( 'Solids' ), expect: true },
+		{ method: 'isFolderItem', value: getItem( 'aeq.jpg' ), expect: false },
+		{ method: 'isFolderItem', value: {}, expect: false },
+
+		{ method: 'isFile', value: new File(), expect: true },
+		{ method: 'isFile', value: getItem( 'aeq.jpg' ), expect: false },
+		{ method: 'isFile', value: getItem( 'Solids' ), expect: false },
+		{ method: 'isFile', value: {}, expect: false },
+
+		{ method: 'isFootageItem', value: getItem( 'aeq.jpg' ), expect: true },
+		{ method: 'isFootageItem', value: getItem( 'Solids' ), expect: false },
+		{ method: 'isFootageItem', value: {}, expect: false },
+
+		{ method: 'isComp', value: comp, expect: true },
+		{ method: 'isComp', value: comp.layer( 1 ), expect: false },
+		{ method: 'isComp', value: {}, expect: false },
 
 		{ method: 'isSolidLayer', value: comp.layer( 'null' ), expect: false },
 		{ method: 'isSolidLayer', value: comp.layer( 'solid' ), expect: true },
@@ -170,9 +203,9 @@ function getTests( comp ) {
 		{ method: 'isFileLayer', value: comp.layer( 'c4d' ), expect: true },
 		{ method: 'isFileLayer', value: null, expect: false },
 
+		{ method: 'isPlaceholder', value: comp.layer( 'placholder' ), expect: true },
 		{ method: 'isPlaceholder', value: comp.layer( 'null' ), expect: false },
 		{ method: 'isPlaceholder', value: comp.layer( 'jpg' ), expect: false },
-		{ method: 'isPlaceholder', value: comp.layer( 'placholder' ), expect: true },
 		{ method: 'isPlaceholder', value: null, expect: false }, // TODO: Fails
 
 		{ method: 'isTrackMatte', value: comp.layer( 'null' ), expect: false },
@@ -190,12 +223,34 @@ function getTests( comp ) {
 		{ method: 'isLayer', value: comp.layer( 'shape' ), expect: true },
 		{ method: 'isLayer', value: null, expect: false },
 
-		// TODO: isShapeLayer
-		// TODO: isTextLayer
-		// TODO: isCameraLayer
-		// TODO: isLightLayer
-		// TODO: isPrecomp
-		// TODO: isLayer
+		{ method: 'isShapeLayer', value: comp.layer( 'av' ), expect: false },
+		{ method: 'isShapeLayer', value: comp.layer( 'null' ), expect: false },
+		{ method: 'isShapeLayer', value: comp.layer( 'shape' ), expect: true },
+		{ method: 'isShapeLayer', value: null, expect: false },
+
+		{ method: 'isTextLayer', value: comp.layer( 'av' ), expect: false },
+		{ method: 'isTextLayer', value: comp.layer( 'null' ), expect: false },
+		{ method: 'isTextLayer', value: comp.layer( 'text' ), expect: true },
+		{ method: 'isTextLayer', value: comp.layer( 'boxtext' ), expect: true },
+		{ method: 'isTextLayer', value: null, expect: false },
+
+		{ method: 'isCameraLayer', value: comp.layer( 'av' ), expect: false },
+		{ method: 'isCameraLayer', value: comp.layer( 'null' ), expect: false },
+		{ method: 'isCameraLayer', value: comp.layer( 'light' ), expect: false },
+		{ method: 'isCameraLayer', value: comp.layer( 'camera' ), expect: true },
+		{ method: 'isCameraLayer', value: null, expect: false },
+
+		{ method: 'isLightLayer', value: comp.layer( 'av' ), expect: false },
+		{ method: 'isLightLayer', value: comp.layer( 'null' ), expect: false },
+		{ method: 'isLightLayer', value: comp.layer( 'camera' ), expect: false },
+		{ method: 'isLightLayer', value: comp.layer( 'light' ), expect: true },
+		{ method: 'isLightLayer', value: null, expect: false },
+
+		{ method: 'isPrecomp', value: comp.layer( 'null' ), expect: false },
+		{ method: 'isPrecomp', value: comp.layer( 'camera' ), expect: false },
+		{ method: 'isPrecomp', value: comp.layer( 'light' ), expect: false },
+		{ method: 'isPrecomp', value: comp.layer( 'comp' ), expect: true },
+		{ method: 'isPrecomp', value: null, expect: false },
 
 		{ method: 'isProperty', value: comp.layer( 1 ).property( 'Position' ), expect: true },
 		{ method: 'isProperty', value: comp.layer( 1 ).property( 'ADBE Transform Group' ), expect: false },
@@ -203,8 +258,9 @@ function getTests( comp ) {
 		{ method: 'isPropertyGroup', value: comp.layer( 1 ).property( 'Position' ), expect: false },
 		{ method: 'isPropertyGroup', value: comp.layer( 1 ).property( 'ADBE Transform Group' ), expect: true },
 
-		{ method: 'isMaskPropertyGroup', value: comp.layer( 1 ).property( 'Position' ), expect: true },
-		{ method: 'isMaskPropertyGroup', value: comp.layer( 1 ).property( 'ADBE Transform Group' ), expect: false }
+		{ method: 'isMaskPropertyGroup', value: comp.layer( 'masked' ).property( 'ADBE Transform Group' ), expect: false },
+		{ method: 'isMaskPropertyGroup', value: comp.layer( 'masked' ).property( 'ADBE Mask Parade' ).property( 'ADBE Mask Atom' ).property( 'ADBE Mask Shape' ), expect: false },
+		{ method: 'isMaskPropertyGroup', value: comp.layer( 'masked' ).property( 'ADBE Mask Parade' ).property( 'ADBE Mask Atom' ), expect: true }
 	];
 }
 }() );
